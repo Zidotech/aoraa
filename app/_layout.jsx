6c530edaaa5f7b516e0577e1b7564384216b04fd
Zulu <zulu.devs@gmail.com>
2024-06-05 16:52:51 +0100

    re-create the project

---
+++ app/_layout.jsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) {
+      console.error("Error loading fonts:", error);
+      return;
+    }
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null; // You can render a placeholder component or loading spinner here if desired.
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* Uncomment the following line if you have the search/[query] screen */}
+      {/* <Stack.Screen name="search/[query]" options={{ headerShown: false }} /> */}
+    </Stack>
+  );
+};
+
+export default RootLayout;
