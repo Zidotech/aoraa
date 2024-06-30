94649ae9e1b02d8c32636a8f2b430810c88b7231
Zulu <zulu.devs@gmail.com>
2024-06-30 16:11:56 +0100

    Added the first look of the homepage

--- components/index.js
+++ components/index.js
@@ -1,4 +1,6 @@
 import CustomButton from "./CustomButton";
 import FormField from "./FormField";
+import SearchInput from "./SearchInput";
+import Trending from "./Trending";
 
-export { CustomButton, FormField };
+export { CustomButton, FormField, SearchInput, Trending };
